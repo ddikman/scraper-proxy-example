3c1a78e30f8cc40ade227367949b036061b09849
David <ddikman@gmail.com>
2022-12-22 01:30:56 +0900

    Cleaned parameters

--- web/api/request.ts
+++ web/api/request.ts
@@ -1,6 +1,16 @@
 
+function removeVercelHeaders(headers) {
+  const newHeaders = {}
+  for (const [key, value] of Object.entries(headers)) {
+    if (!key.startsWith('x-vercel-')) {
+      newHeaders[key] = value
+    }
+  }
+  return newHeaders
+}
+
 export default async function handler(request: any, response: any) {
-    console.log({ 'body': request.body, 'query': request.query, 'headers': request.headers })
+    console.log({ 'body': request.body ?? '', 'query': { ...request.query }, 'headers': removeVercelHeaders(request.headers) })
     response.setHeader('Access-Control-Allow-Origin', '*')
     return response.end("Saved!");
   }
